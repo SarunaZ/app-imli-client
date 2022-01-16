8e6b29f8852b14dcd05c9c1ccc4671cf81e84b07
Sarunas Zube <sarunas.zube@gmail.com>
2022-01-16 14:29:05 +0200

    - added stylling for sidebar toggling

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,12 +1,35 @@
 import style from './style.module.scss';
 import SidebarContent from './SidebarContent';
 import SidebarFooter from './SidebarFooter';
+import classnames from 'classnames';
+import { useState } from 'react';
+import { ReactComponent as RightArrow } from 'Images/icons/arrow-right.svg';
 
-const Sidebar = () => (
-  <aside className={style.sidebarWrapper}>
-    <SidebarContent />
-    <SidebarFooter />
-  </aside>
-);
+const Sidebar = () => {
+  const [isSidebarShow, setSidebarShow] = useState<boolean>(false);
+  const toggleSiderbar = () => setSidebarShow(prev => !prev);
+  const sidebarClasses = classnames(style.sidebarWrapper, {
+    [style.active]: isSidebarShow  
+  });
+  const siderbarToggleClasses = classnames(style.sidebarToggle, {
+    [style.active]: isSidebarShow  
+  });
+  
+  return (
+    <aside className={sidebarClasses}>
+      <div className={style.siderbar}>
+        <SidebarContent />
+        <SidebarFooter />
+        <button 
+          className={siderbarToggleClasses} 
+          onClick={toggleSiderbar}
+        >
+          <RightArrow height="25px" />
+        </button>
+      </div>
+    </aside>
+  );
+} 
+
 
 export default Sidebar;
