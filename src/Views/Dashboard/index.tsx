f96b4895027202c8d04933b7fa15d962aa7521e7
Sarunas Zube <sarunas.zube@gmail.com>
2022-01-19 15:53:38 +0200

    - updated manifesto logos and icons

--- src/Views/Dashboard/index.tsx
+++ src/Views/Dashboard/index.tsx
@@ -1,11 +1,12 @@
 import { AuthenticationProvider } from "Providers/Authentication/Authentication";
 import { useContext } from "react";
+import style from './style.module.scss';
 
 const Dashboard = () => {
   const { username } = useContext(AuthenticationProvider);
   
   return (
-    <h1>
+    <h1 className={style.dashboardTitle}>
       Hello, {username}
     </h1>
   );
