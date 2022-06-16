a8502b99eb4b71a936a7c690fe22a74f6f2b865d
Sarunas Zube <sarunas.zube@gmail.com>
2022-06-16 21:07:59 +0300

    - removing surprise

--- src/Views/Dashboard/index.tsx
+++ src/Views/Dashboard/index.tsx
@@ -11,20 +11,6 @@ const Dashboard = () => {
       <h1 className={style.dashboardTitle}>
         Hello, {username}
       </h1>
-      <div className={style.suprise}>
-        <h2>{'Welcome back, my love!'}</h2>
-        <form>
-          <label>Please enter YOUR favourite password</label>
-          <input type="text" onInput={(e: SyntheticEvent<HTMLInputElement>) => setInputValue(e.currentTarget.value)} />
-        </form>
-        {inputValue === '10pyrageliu' && (
-          <div className={style.supriseShow}>
-            <h3>{'Congratulations'}</h3>
-            <img src="https://c.tenor.com/KVosJDEqP_UAAAAC/bo-burnham-inside.gif" alt="" />
-            <h3 className={style.supriseShowText}>{'Your surprise is in a Cold bag'}</h3>
-          </div>
-        )}
-      </div>
     </>
   );
 };
