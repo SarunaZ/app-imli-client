0ae728079c042eb835d4b03b987b7e70c303055e
Sarunas Zube <sarunas.zube@gmail.com>
2023-11-04 14:28:02 +0200

    small changes in eslint

--- src/Schema/queries/mealQueries.ts
+++ src/Schema/queries/mealQueries.ts
@@ -2,7 +2,10 @@ import { gql } from "@apollo/client";
 import { MealListQuery, MealListQueryVariables } from "Schema/types";
 import { QueryDocument } from "Utilities/typesExport";
 
-export const MEAL_LIST_DATA: QueryDocument<MealListQuery, MealListQueryVariables> = gql`
+export const MEAL_LIST_DATA: QueryDocument<
+  MealListQuery,
+  MealListQueryVariables
+> = gql`
   query mealList {
     meals {
       id
