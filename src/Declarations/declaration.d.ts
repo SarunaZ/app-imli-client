70db0386649a7648193b02db40c78e7759acbfbb
Sarunas Zube <sarunas.zube@gmail.com>
2023-12-22 14:07:38 +0200

    - Adding changes

--- src/Declarations/declaration.d.ts
+++ src/Declarations/declaration.d.ts
@@ -8,3 +8,8 @@ declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
