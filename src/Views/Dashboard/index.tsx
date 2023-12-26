174953c7fef0f964aede815af3f21bd34ecb1cd5
Šarūnas Zubė <sarunas.zube@gmail.com>
2023-12-26 10:26:45 +0200

    Revert "- Adding changes"

--- src/Views/Dashboard/index.tsx
+++ src/Views/Dashboard/index.tsx
@@ -1,40 +1,22 @@
-import React, { SyntheticEvent } from "react";
+import React from "react";
 import style from "./style.scss";
-import Welcome from "./welcome.gif";
-import useState from "Hooks/useState";
-
-interface State {
-  inputString: string;
-  correctAnswer: string;
-}
+import { DASHBOARD_DATA } from "Schema/queries/dashboardQueries";
+import Loader from "Components/Loader";
+import ErrorHandler from "Components/ErrorHandler";
+import { useQuery } from "@apollo/client/react/hooks/useQuery";
 
 const Dashboard = () => {
-  const [state, setState] = useState<State>({
-    inputString: "",
-    correctAnswer: "RPIS",
-  });
+  const { loading, error, data = {} } = useQuery(DASHBOARD_DATA);
 
-  const handleInput = (event: SyntheticEvent<HTMLInputElement>) => {
-    setState({ inputString: event.currentTarget.value });
-  };
-
-  const isCorrect =
-    state.inputString === state.correctAnswer ||
-    state.inputString === state.correctAnswer.toLowerCase();
+  if (loading) return <Loader />;
+  if (error) return <ErrorHandler error={error} />;
 
   return (
-    <div className={style.dashboard}>
-      <h1 className={style.dashboardTitle}>Hello, Bubu</h1>
-      <img src={Welcome} />
-
-      <h2 className={style.dashboardTitle}>Enter first four characters</h2>
-      {!isCorrect && (
-        <input maxLength={4} value={state.inputString} onInput={handleInput} />
-      )}
-      {isCorrect && (
-        <span className={style.answer}>{"Spicy date-night teddy"}</span>
-      )}
-    </div>
+    <>
+      <h1 className={style.dashboardTitle}>
+        Hello, {data.userDashboard?.username}
+      </h1>
+    </>
   );
 };
 
