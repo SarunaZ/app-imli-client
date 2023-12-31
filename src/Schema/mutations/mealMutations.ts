c8aa1538ef7330073122b083ff6b5bb625217d85
Sarunas Zube <sarunas.zube@gmail.com>
2023-12-31 17:45:08 +0200

    - add interfaces to product  and meal graphql types

--- src/Schema/mutations/mealMutations.ts
+++ src/Schema/mutations/mealMutations.ts
@@ -1,6 +1,14 @@
 import { gql } from "@apollo/client";
+import {
+  MealMutationMutation,
+  MealMutationMutationVariables,
+} from "Schema/types";
+import { MutationDocument } from "Utilities/typesExport";
 
-export const MEAL_NAME_MUTATION = gql`
+export const MEAL_NAME_MUTATION: MutationDocument<
+  MealMutationMutation,
+  MealMutationMutationVariables
+> = gql`
   mutation mealMutation(
     $id: String
     $name: String!
