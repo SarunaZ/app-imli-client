66ca5e986aa369bcb4b3d795980833d75958f52a
Sarunas Zube <sarunas.zube@gmail.com>
2024-01-15 18:34:20 +0200

    - adding root path to webpack config

--- internals/webpack/webpack.production.config.js
+++ internals/webpack/webpack.production.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WebpackObfuscator = require('webpack-obfuscator');
+const WebpackObfuscator = require("webpack-obfuscator");
 const CopyPlugin = require("copy-webpack-plugin");
+
+const ROOT_PATH = "../..";
 
 module.exports = () => ({
   output: {
@@ -38,7 +40,7 @@ module.exports = () => ({
             loader: "sass-loader",
             options: {
               sourceMap: false,
-              additionalData: "@import \"src/Styles/variables.scss\";",
+              additionalData: '@import "src/Styles/variables.scss";',
               sassOptions: {
                 outputStyle: "compressed",
               },
@@ -60,10 +62,13 @@ module.exports = () => ({
         },
       ],
     }),
-    new WebpackObfuscator({
-      rotateStringArray: true,
-      reservedStrings: [ '\s*' ],
-    }, ['*node_modules*']),
+    new WebpackObfuscator(
+      {
+        rotateStringArray: true,
+        reservedStrings: ["s*"],
+      },
+      ["*node_modules*"],
+    ),
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
       chunkFilename: "[id].[contenthash].css",
@@ -72,16 +77,16 @@ module.exports = () => ({
   optimization: {
     minimize: true,
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
+          name: "vendor",
+          chunks: "all",
           minChunks: 2,
-          minSize: 0
-        }
-      }
+          minSize: 0,
+        },
+      },
     },
     minimizer: [
       new CssMinimizerPlugin({
